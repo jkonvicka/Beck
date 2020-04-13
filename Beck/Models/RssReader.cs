98763143321e507dbeedefc1378d8d481e835601
jkonvicka <konvicka.g630@gmail.com>
2020-04-13 11:55:02 +0200

    homepage, added rss reader for reading news

---
+++ Beck/Models/RssReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Xml;
+
+namespace Beck.Models
+{
+    public class RssReader
+    {
+        private static XmlReader rssReader;
+        private static SyndicationFeed newsFeed;
+        public static List<SyndicationItem> GetNews(string url, int take)
+        {
+            rssReader = XmlReader.Create(url);
+            newsFeed = SyndicationFeed.Load(rssReader);
+            return (newsFeed.Items.OrderByDescending(i => i.PublishDate).Take(take).ToList());
+        }
+
+        public static List<SyndicationItem> GetNews(string url)
+        {
+            rssReader = XmlReader.Create(url);
+            newsFeed = SyndicationFeed.Load(rssReader);
+            return (newsFeed.Items.OrderByDescending(i => i.PublishDate).ToList());
+        }
+    }
+}
